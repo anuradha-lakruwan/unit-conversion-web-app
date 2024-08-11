5ff184962b96747a545dc18c6ca7a2c34a843827
Anuradha Lakruwan <128252764+anuradha-lakruwan@users.noreply.github.com>
2024-08-11 14:32:03 +0530

    set xs to have two per row

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,15 +4,14 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Media from "react-bootstrap/Media";
 import { chunkArray } from "../../utils";
 import conversionData from "../../data/conversions.json";
 import Conversion from "../../types/Conversion";
 import { useTranslation } from "react-i18next";
 
-const xs_size: number = 1;
+const xs_size: number = 2;
 const sm_size: number = 2;
-const md_size: number = 3;
+const md_size: number = 2;
 const lg_size: number = 4;
 
 const ConversionCard = (props: { category: string; icon_url: string }) => {
@@ -21,21 +20,14 @@ const ConversionCard = (props: { category: string; icon_url: string }) => {
   return (
     <Col>
       <Card border="success" className="mb-3">
-        <Card.Body>
-          <Media>
-            <img
-              width={64}
-              height={64}
-              className="align-self-start mr-3"
-              src={props.icon_url}
-              alt={t(props.category)}
-            />
-            <Media.Body>
-              <Button className="stretched-link" variant="success">
-                {t(props.category)}
-              </Button>
-            </Media.Body>
-          </Media>
+        {/* <Card.Img variant="top" src={props.icon_url} /> */}
+        <Card.Title className="text-center mb-0 mt-1">
+          <img src={props.icon_url} />
+        </Card.Title>
+        <Card.Body className="text-center">
+          <Button className="stretched-link" variant="success">
+            {t(props.category)}
+          </Button>
         </Card.Body>
       </Card>
     </Col>
