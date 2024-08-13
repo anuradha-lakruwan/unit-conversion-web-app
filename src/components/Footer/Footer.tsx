cbc7207915051c25ec529fe22bf55b15fa6c823f
Anuradha Lakruwan <128252764+anuradha-lakruwan@users.noreply.github.com>
2024-08-13 09:59:03 +0530

    updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
     return (
     <footer className="pt-5 text-muted text-center text-small">
-        <p className="mb-1">icons by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a>, source code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/ian-hamlin/unit-converter">GitHub</a></p>
+        <p className="mb-1">icons by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a>, source code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/anuradha-lakruwan/unit-conversion-web-app">GitHub</a></p>
         <p>
             {i18n.language === "es" ?
             <Button onClick={() => changeLanguage("en")}variant="link">{t("Lang_English")}</Button>
