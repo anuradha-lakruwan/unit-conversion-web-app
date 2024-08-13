ffa188caebfb8c229b59bb1aa324f4d8dd365b9f
Anuradha Lakruwan <128252764+anuradha-lakruwan@users.noreply.github.com>
2024-08-13 04:24:02 +0000

    Updates

---
+++ precache-manifest.387c8ed3c778c8e74aa9a783e5f8aafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ede26e417d2eeba2a9169902a262fc9",
+    "url": "/unit-conversion-web-app/index.html"
+  },
+  {
+    "revision": "18f1ff079311fd9a2715",
+    "url": "/unit-conversion-web-app/static/js/2.25b5cac7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/unit-conversion-web-app/static/js/2.25b5cac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67e5448902b427bda26",
+    "url": "/unit-conversion-web-app/static/js/main.494d19c6.chunk.js"
+  },
+  {
+    "revision": "7ff3af7b6e345f37a788",
+    "url": "/unit-conversion-web-app/static/js/runtime-main.6c0ef304.js"
+  }
+]);
